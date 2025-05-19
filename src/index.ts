515adb2aa39ff992126254ba88350c638c24fc74
Marcelo <molandim@gmail.com>
2025-05-19 14:55:38 +0100

    Initialize Segment API script with environment configuration, API functions, and filter management. Add TypeScript support and necessary dependencies. Create initial filter definitions for event handling.

---
+++ src/index.ts
@@ -0,0 +1,62 @@
+import "dotenv/config";
+import {
+  createDestinationFilter,
+  FiltersInDestination,
+  getDestination,
+  getDestinationFilters,
+  updateDestinationFilter,
+} from "./api";
+import { processEnvSchema, type Filter } from "./schemas";
+import { DESTINATIONS_DEV, DESTINATIONS_PROD } from "./const";
+import { loadFilters } from "./load-filters";
+
+const { ENV } = processEnvSchema.parse(process.env);
+
+const destinations = ENV === "dev" ? DESTINATIONS_DEV : DESTINATIONS_PROD;
+
+async function upsertFilter({
+  filtersInDestination,
+  filter,
+  destinationId,
+  sourceId,
+}: {
+  filtersInDestination: FiltersInDestination;
+  filter: Filter;
+  destinationId: string;
+  sourceId: string;
+}) {
+  const filterInDestination = filtersInDestination.data.filters.find(
+    (f) => f.title === filter.title
+  );
+  if (filterInDestination) {
+    console.log(`Updating filter ${filter.title}`);
+    await updateDestinationFilter(destinationId, sourceId, filter);
+  } else {
+    console.log(`Creating filter ${filter.title}`);
+    await createDestinationFilter(destinationId, sourceId, filter);
+  }
+}
+
+async function main() {
+  for (const [name, id] of Object.entries(destinations)) {
+    const { data: destinationDetails } = await getDestination(id);
+    const sourceId = destinationDetails.destination.sourceId;
+
+    const filtersInDestination = await getDestinationFilters(id);
+
+    const filtersToSync = await loadFilters();
+
+    await Promise.all(
+      filtersToSync.map((filter) =>
+        upsertFilter({
+          filtersInDestination,
+          filter,
+          destinationId: id,
+          sourceId,
+        })
+      )
+    );
+  }
+}
+
+main();
