ad0b720c5ac5255281579a6d4041282a7e881229
Marcelo <molandim@gmail.com>
2025-05-27 15:33:33 +0100

    Add VSCode settings for color customization, remove unused filters, and add new segment event definitions

--- src/const.ts
+++ src/const.ts
@@ -1,12 +1,10 @@
 export const API_BASE_URL = "https://api.segmentapis.com";
 
 export const DESTINATIONS_DEV = {
-  "SOURCE:_.NET_Dev_DEST:_Bloomreach": "67c1e4a8985b6b074083caa4",
-  "SOURCE:_.Net_Mktng_AutoServ(Dev)_DEST:_Bloomreach":
-    "67c1e6b44260ced851878eef",
-  "SOURCE:_Android_Dev_DEST:_Bloomreach": "67c1e3bfe688ef7dbc89d8f8",
-  "SOURCE:_IOS_Dev_DEST:_Bloomreach": "67c1e579b41222b201f28afb",
-
+  // "SOURCE:_.NET_Dev_DEST:_Bloomreach": "67c1e4a8985b6b074083caa4",
+  // "SOURCE:_.Net_Mktng_AutoServ(Dev)_DEST:_Bloomreach": "67c1e6b44260ced851878eef",
+  // "SOURCE:_Android_Dev_DEST:_Bloomreach": "67c1e3bfe688ef7dbc89d8f8",
+  // "SOURCE:_IOS_Dev_DEST:_Bloomreach": "67c1e579b41222b201f28afb",
   "SOURCE:_Javascript_Dev_DEST:_Bloomreach": "67c1e2317e156ee49427cdf4",
 };
 
@@ -14,6 +12,7 @@ export const DESTINATIONS_PROD = {
   "SOURCE:_ReactNativeAppProd_DEST:_Bloomreach": "67ff7e0a66d10a57eff83029",
   "SOURCE:_.NET_Live_DEST:_Bloomreach": "67b367032d559f77f4ff9838",
   "SOURCE:_.Net_Mktng_AutoServ_DEST:_Bloomreach": "67b498fff94674df428279b7",
+  "SOURCE:_Android_Live_DEST:_Bloomreach": "67b34a2d859096e48c67f11a",
   "SOURCE:_IOS_Live_DEST:_Bloomreach": "67af8a04a55dfa5ca8d322ca",
   "SOURCE:_Javascript-V5_DEST:_Bloomreach": "67b355ef20b5d8bfd4c65953",
 };
